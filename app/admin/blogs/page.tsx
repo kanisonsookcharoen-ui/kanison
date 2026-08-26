c197ac18f0fe9de341f4b2751609f65040881ab8
kanisonsookcharoen <kanison.sookcharoen@gmail.com>
2026-08-26 14:56:42 +0700

    update

--- app/admin/blogs/page.tsx
+++ app/admin/blogs/page.tsx
@@ -1,9 +1,10 @@
 import BlogForm from "@/components/ฺBlogForm";
-
+import BlogCard from "@/components/BlogCard";
 export default function BlogPage() {
     return (
         <div>
            <BlogForm/>
+           <BlogCard/>
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image";
+import BlogCard from "@/components/BlogCard";
 export default function Home() {
   return (
-    <div>
-      <h2>ยินดีต้องเข้าสู่เว็บไซต์</h2>
-      <p>โดย คณิศร สุขเจริญ</p>
+    <div >
+    
+      <h2> ยินดีต้อนรับเข้าสู่เว็บไซต์ </h2>
+      <p> โดย สมเกียรติ ใจดี </p>
+    <BlogCard/>
     </div>
   );
 }
